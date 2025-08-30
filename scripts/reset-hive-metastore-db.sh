6f58af3d605a66b7f525636515a38cf4ee2f3832
Kevin Kim <kimanikevin254@gmail.com>
2025-08-30 14:17:19 +0300

    remove the --build flag from script

--- scripts/reset-hive-metastore-db.sh
+++ scripts/reset-hive-metastore-db.sh
@@ -8,4 +8,4 @@ PROJECT_NAME=$(basename $(pwd))
 docker volume rm ${PROJECT_NAME}_hive-metastore-db-data 2>/dev/null || true
 
 echo "Starting services..."
-docker compose -f datalake-docker-compose.yml up -d --build
+docker compose -f datalake-docker-compose.yml up -d
